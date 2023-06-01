fc219ee39e4490709a5c7fd63d68f42f3dd1300c
Lenin Acosta <rkb.lenin@gmail.com>
2023-05-31 21:52:13 -0500

    integro matplotlib con qt

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-from PyQt5 import QtWidgets, uic
-from toolkit import seleccionar_archivo
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QFileDialog
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-app = QtWidgets.QApplication([])
-ventana = uic.loadUi('gui_main.ui')
+class VentanaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gui_main.ui', self)
+        self.pushButton_cargar_img.clicked.connect(self.seleccionar_archivo)
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.verticalLayout_img_original.addWidget(self.canvas)
 
-ventana.pushButton_cargar_img.clicked.connect(seleccionar_archivo)
+    def seleccionar_archivo(self):
+        archivo = QFileDialog()
+        archivo.setWindowTitle("Seleccionar archivo")
+        archivo.setFileMode(QFileDialog.ExistingFile)
+        if archivo.exec_():
+            ruta = archivo.selectedFiles()
+            print(ruta[0])
+            self.leer_img(ruta[0])
 
-# ejecutar
+    def leer_img(self, ruta):
+        img = cv2.imread(ruta)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        ax.imshow(img_rgb)
+        ax.axis('off')
+        self.canvas.draw()
+
+app = QApplication([])
+ventana = VentanaPrincipal()
 ventana.show()
 app.exec()
--- toolkit.py
+++ toolkit.py
@@ -1,11 +1,18 @@
 from PyQt5.QtWidgets import QFileDialog
 import cv2
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+class Canvas_dibujarimg(FigureCanvas):
+    def __init__(self, parent=None):
+        self.fig = plt.imshow()
+        super().__init__(self.fig)
 
 def leer_img(ruta):
     img = cv2.imread(ruta)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(img_rgb)
+    plt.axis('off')
     plt.show()
 
 def seleccionar_archivo():
